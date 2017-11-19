72e8a6ee70c86d75cb570a6c861f895bb9b7932c
Jake Foraker <jakeforaker83@gmail.com>
2017-11-19 12:16:31 -0500

    update now url

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-const url = process.env.NODE_ENV === 'production' ? 'https://quickapi-xsgsfnerwg.now.sh' : 'http://localhost:3000';
+const url = process.env.NODE_ENV === 'production' ? 'https://react-quick-api-server-gdvaihwxwt.now.sh' : 'http://localhost:3000';
 export default url
